d84bed65a22656d2ec6ec4bbb5b1fade12a32cdf
Anthony-Willis <anthonywillis1997@outlook.com>
2022-09-26 15:43:38 -0700

    Lab 5.2 part1

--- Lab05.2/src/com/javatunes/personnel/EmployeeFactory.java
+++ Lab05.2/src/com/javatunes/personnel/EmployeeFactory.java
@@ -12,20 +12,32 @@ import java.sql.Date;
 import java.util.Map;
 
 public class EmployeeFactory {
-  
+
   // prevent direct instantiation - this is an all-static factory class
   private EmployeeFactory() {
   }
-  
+
   /**
    * TODO: given the input map, create and return the correct object (with its properties set).
    * If the input map's "type" value is not "HE" or "SE", throw IllegalArgumentException with a suitable message.
    */
-  public static Employee createEmployee(Map<String,String> inputMap)
-  throws IllegalArgumentException {
+  public static Employee createEmployee(Map<String, String> inputMap)
+      throws IllegalArgumentException {
     // return value
     Employee emp = null;
-    
+    String type= inputMap.get("type");
+    if (!inputMap.get("type").equals("SE") || !inputMap.get("type").equals("HE")) {
+      throw new IllegalArgumentException("Employee must be hourly or salary");
+    }else {
+      String nameOfEmployee = inputMap.get("name");
+      Date hireDateOfEmployee = Date.valueOf(inputMap.get("hireDate"));
+    }
+    //OUR CODE GOES HERE
+    // TODO: 9/26/2022 if type is not equal to HE or SE, then throw illegalArgExcept
+    // TODO: 9/26/2022 read the name and hire date from input map. All values are strings
+    // TODO: 9/26/2022 if equal to HE or SE then read and parse rate/hours and salary inputMap 
+    // TODO: 9/26/2022 create and return an insatnce of hourly/salary emp with values read 
+
     return emp;
   }
 }
